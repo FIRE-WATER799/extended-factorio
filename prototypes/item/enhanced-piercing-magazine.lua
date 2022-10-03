566234a51dcf92dec843424ed8dbac488770c3f5
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-10-03 12:41:00 -0500

    Added normal and firearm items

---
+++ prototypes/item/enhanced-piercing-magazine.lua
@@ -0,0 +1,33 @@
+data:extend(
+  {
+    {
+      type = "ammo",
+      name = "enhanced-piercing-magazine",
+      icon = "__Extended-Factorio__/graphics/items/ammo/enhanced-piercing-magazine.png",
+      icon_mipmaps = 4,
+      icon_size = 32,
+      ammo_type =
+      {
+        category = "bullet",
+        action =
+        {
+          type = "direct",
+          action_delivery =
+          {
+            type = "instant",
+            target_effects =
+            {
+              {
+                type = "damage",
+                damage = { amount = 13, type = "physical" }
+              }
+            }
+          }
+        }
+      },
+      magazine_size = 10,
+      group = "combat",
+      subgroup = "ammo",
+      stack_size = 200
+    }
+  })

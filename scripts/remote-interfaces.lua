ad70a7c815cdf6a76cda697387c506af534afa96
FIRE-WATER799 <57648521+FIRE-WATER799@users.noreply.github.com>
2022-12-12 14:45:14 +0000

    Fix Whitespace

--- scripts/remote-interfaces.lua
+++ scripts/remote-interfaces.lua
@@ -1,5 +1,5 @@
 remote.add_interface("Extended-Factorio", {
-    jetpack_fuels = function() 
+    jetpack_fuels = function()
         return {["basic-fuel-capsule"] = 0.6, ["enhanced-fuel-capsule"] = 0.9, ["reinforced-fuel-capsule"] = 1, ["atomic-fuel-capsule"] = 1.2}
     end
 })

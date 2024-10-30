d3abf6d6b72a7bbb8d1930a6a4e8efe793ba0e46
FIRE-WATER799 <haywood2008918@outlook.com>
2024-10-29 19:34:59 -0500

    found you you stupid comma

--- prototypes/item/atomic-artillery.lua
+++ prototypes/item/atomic-artillery.lua
@@ -5,7 +5,29 @@ data:extend(
 		name = "atomic-artillery",
 		icon = "__Extended-Factorio__/graphics/items/atomic-artillery.png",
 		icon_size = 32,
-		ammo_category = "artillery-shell"
+		ammo_category = "artillery-shell",
+		ammo_type =
+{
+			category = "artillery-shell",
+			target_type = "position",
+			action =
+			{
+				type = "direct",
+				action_delivery =
+				{
+					type = "artillery",
+					projectile = "atomic-artillery",
+					starting_speed = 1,
+					direction_deviation = 0,
+					range_deviation = 0,
+					source_effects =
+					{
+						type = "create-explosion",
+						entity_name = "artillery-cannon-muzzle-flash"
+					},
+				},
+			},
+		},
 		subgroup = "extfac-ammo",
 		order = "d[explosive-cannon-shell]-d[artillery]",
 		stack_size = 1

2c282655e24429416665fea893c962f6c6fe3c8d
FIRE-WATER799 <haywood2008918@outlook.com>
2024-10-29 19:33:53 -0500

    Idk

--- prototypes/item/atomic-artillery.lua
+++ prototypes/item/atomic-artillery.lua
@@ -5,28 +5,7 @@ data:extend(
 		name = "atomic-artillery",
 		icon = "__Extended-Factorio__/graphics/items/atomic-artillery.png",
 		icon_size = 32,
-		ammo_category = "artillery_shell"
-		ammo_type = {
-			category = "artillery-shell",
-			target_type = "position",
-			action =
-			{
-				type = "direct",
-				action_delivery =
-				{
-					type = "artillery",
-					projectile = "atomic-artillery",
-					starting_speed = 1,
-					direction_deviation = 0,
-					range_deviation = 0,
-					source_effects =
-					{
-						type = "create-explosion",
-						entity_name = "artillery-cannon-muzzle-flash"
-					},
-				},
-			},
-		},
+		ammo_category = "artillery-shell"
 		subgroup = "extfac-ammo",
 		order = "d[explosive-cannon-shell]-d[artillery]",
 		stack_size = 1
